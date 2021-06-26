d40452ea0951494efbb984545bbfe60609a446ff
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-06-25 18:05:22 -0600

    update on register and recaptcha

--- web/routes/register_routes.py
+++ web/routes/register_routes.py
@@ -1,9 +1,17 @@
-from flask import render_template
+from flask import render_template, request, redirect
+from tools.recaptcha_helper import RecaptchaHelper
 
 
 class RegisterRoutes:
     @staticmethod
     def configure_routes(app):
-        @app.route("/register")
+        @app.route("/register", methods=["GET", "POST"])
         def register():
-            return render_template("register.html")
+            if request.method == "GET":
+                return render_template("register.html")
+            elif request.method == "POST":
+                recHelper = RecaptchaHelper(request)
+                if recHelper.validateRecaptcha():
+                    return "register valid post"
+                else:
+                    redirect("register")
