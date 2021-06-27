05a5d95ae8f8413951472cfcbabcc4b69fe4cb74
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-06-26 23:05:41 -0600

    update country integration between web and restapi

--- web/static/js/dashboard.js
+++ web/static/js/dashboard.js
@@ -3,3 +3,10 @@ function cityIdOnChange() {
     var cityLink = document.getElementById("cityLink");
     cityLink.href = "/dashboard/city/" + cityId;
 }
+
+function countryCityIdOnChange() {
+    var countryCityId = document.getElementById("countrycityid").value;
+    var countryLink = document.getElementById("countryLink");
+    countryLink.href = "/dashboard/country/" + countryCityId;
+
+}
